61636a9c25478b4b3f6dda84a43f4153fa224947
Badu <david77115@gmail.com>
2020-02-18 16:57:17 +0800

    feat: create 2 different build data processes

--- app/src/main/java/com/crazyma/diffsample/MainActivity.kt
+++ app/src/main/java/com/crazyma/diffsample/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setupRecyclerView()
-        populateData()
+        populateData2()
     }
 
     private fun setupRecyclerView() {
@@ -25,7 +25,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun populateData() {
+    /**
+     * 1. create 1 cell
+     * 2. create several cells and insert to position 0 sequentially
+     */
+    private fun populateData1() {
         GlobalScope.launch(Dispatchers.IO) {
             val list = mutableListOf<MyAdapter.Item>().apply {
                 for (i in 0 until 1) {
@@ -46,9 +50,6 @@ class MainActivity : AppCompatActivity() {
                 for (i in 100 until 120) {
                     add(0, MyAdapter.Item(i, "Created in second loop"))
                 }
-//                for(i in 200 until 203){
-//                    add(15, MyAdapter.Item(i, "Created in second loop"))
-//                }
             }
 
             withContext(Dispatchers.Main) {
@@ -58,4 +59,50 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    /**
+     * 1. create 50 cells
+     * 2. create several cells insert after first cell on odd positions
+     * 3. create several cells insert before first cell
+     */
+    private fun populateData2() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val list = mutableListOf<MyAdapter.Item>().apply {
+                for (i in 0 until 50) {
+                    add(MyAdapter.Item(i, "Created in first loop"))
+                }
+            }
+
+            withContext(Dispatchers.Main) {
+                (recyclerView.adapter as MyAdapter).items = list
+            }
+
+            delay(2000)
+
+            val list2 = mutableListOf<MyAdapter.Item>().apply {
+                list.forEach {
+                    add(it)
+                }
+
+                var x = 1
+                for(i in 100 until 105){
+                    val randomPosition = x
+                    add(randomPosition, MyAdapter.Item(i, "Created in second loop"))
+                    x += 2
+                }
+
+                for (i in 200 until 205) {
+                    add(0, MyAdapter.Item(i, "Created in second loop"))
+                }
+            }
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "updated!", Toast.LENGTH_LONG).show()
+                (recyclerView.adapter as MyAdapter).items = list2
+            }
+
+        }
+    }
+
+
 }
